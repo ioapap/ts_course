f3cbd2c0dfed0486676735433de4978bdaef8a48
Ioannis <powerfulguy4774@gmail.com>
2023-08-29 06:22:12 +0200

    Constructor Functions & The "this" Keyword

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,13 @@
 "use strict";
-class Departement {
+class Department {
     constructor(n) {
         this.name = n;
     }
+    describe() {
+        console.log("Department: " + this.name);
+    }
 }
-const accounting = new Departement("Accounting");
-console.log(accounting);
+const accounting = new Department("Accounting");
+accounting.describe();
+const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+accountingCopy.describe();
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
-class Departement {
+class Department {
   name: string;
 
   constructor(n: string) {
     this.name = n;
   }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
 }
 
-const accounting = new Departement("Accounting");
+const accounting = new Department("Accounting");
 
-console.log(accounting);
+accounting.describe();
+
+const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+
+accountingCopy.describe();
