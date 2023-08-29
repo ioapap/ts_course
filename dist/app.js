94cf6f42804d410fac67274cd847757288a86774
Ioannis <powerfulguy4774@gmail.com>
2023-08-29 11:46:05 +0200

    Generic Classes

--- dist/app.js
+++ dist/app.js
@@ -2,20 +2,43 @@
 function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
-const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 console.log(mergedObj);
 function countAndDescribe(element) {
-    let descriptionText = "Got no value.";
+    let descriptionText = 'Got no value.';
     if (element.length === 1) {
-        descriptionText = "Got 1 element.";
+        descriptionText = 'Got 1 element.';
     }
     else if (element.length > 1) {
-        descriptionText = "Got " + element.length + " elements.";
+        descriptionText = 'Got ' + element.length + ' elements.';
     }
     return [element, descriptionText];
 }
-console.log(countAndDescribe(["Sports", "Cooking"]));
+console.log(countAndDescribe(['Sports', 'Cooking']));
 function extractAndConvert(obj, key) {
-    return "Value: " + obj[key];
+    return 'Value: ' + obj[key];
 }
-extractAndConvert({ name: "Max" }, "name");
+extractAndConvert({ name: 'Max' }, 'name');
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+const numberStorage = new DataStorage();
